2beaee53860fb1d9f29285d177ee049001fe71f8
meghana <maggieguna124@gmail.com>
2025-03-27 17:59:36 +0530

    changes

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import Hero from '../Components/Hero/Hero'
-import Popular from '../Components/Popular/Popular'
-import { Offers } from '../Components/Offers/Offers'
-import { NewCollections } from '../Components/NewCollections/NewCollections'
+import React from 'react';
+import Hero from '../Components/Hero/Hero';
+import Popular from '../Components/Popular/Popular';
+import { Offers } from '../Components/Offers/Offers';
+import { NewCollections } from '../Components/NewCollections/NewCollections';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
 
 export default function Shop() {
   return (
@@ -11,6 +12,7 @@ export default function Shop() {
       <Popular/>
       <Offers/>
       <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
